b33165eaeda794622e761e7afea561333a1aa0a5
HUMEROUSSSS <112653260+HUMEROUSSSS@users.noreply.github.com>
2022-09-02 13:43:16 -0400

    Update streamlit_app.py
    sdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
 	streamlit.error()
 
 #import
-streamlit.header("The fruit load list contains:)
+streamlit.header("The fruit load list contains:")
 
 def get_fruit_load_list():
 	with my_cnx.cursor() as my_cur:
